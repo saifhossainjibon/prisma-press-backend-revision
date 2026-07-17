d35883885be03b172e92ec067f617718bbc54c3e
saifhossainjibon <saifhossainjibon@gmail.com>
2026-07-17 23:31:01 +0600

    added global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import cookieParser from "cookie-parser";
-import express, { Application, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import cors from "cors";
 import config from "./config";
 import { userRoutes } from "./modules/user/user.route";
 import { authRoutes } from "./modules/auth/auth.route";
 import { postRoutes } from "./modules/post/post.route";
 import { commentRoutes } from "./modules/comment/comment.route";
+import { notFound } from "./middlewares/notFound";
+import { globalErrorHandler } from "./utils/globalErrorHandler";
+
 
 const app: Application = express();
 // here is the middleware we have been used
@@ -27,5 +30,6 @@ app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
-
+app.use(notFound)
+app.use(globalErrorHandler)
 export default app;
--- src/utils/catchAsync.ts
+++ src/utils/catchAsync.ts
@@ -6,13 +6,7 @@ export const catchAsync = (fn: RequestHandler) => {
     try {
       await fn(req, res, next);
     } catch (error) {
-      console.log(error);
-      res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
-        success: false,
-        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
-        message: "Failed to register user",
-        error: (error as Error).message,
-      });
+      next(error)
     }
   };
 };
