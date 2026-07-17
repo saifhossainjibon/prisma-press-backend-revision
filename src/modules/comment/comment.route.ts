922217fe54d4f4b2724c0eb1bf0b1991e83c68c6
saifhossainjibon <saifhossainjibon@gmail.com>
2026-07-17 18:57:43 +0600

    comment api done

--- src/modules/comment/comment.route.ts
+++ src/modules/comment/comment.route.ts
@@ -5,17 +5,17 @@ import { commentController } from "./comment.controller";
 
 const router = Router();
 
-router.post("/",auth(Role.USER, Role.ADMIN, Role.AUTHOR),commentController.createComment);
+router.post("/", auth(Role.USER, Role.ADMIN, Role.AUTHOR), commentController.createComment);
 
-router.get("/author/:authorId",commentController.getCommentByAuthorId);
+router.get("/author/:authorId", commentController.getCommentByAuthorId);
 
-router.get("/:commentId",commentController.getCommentByCommentId);
+router.get("/:postId", commentController.getCommentByPostId);
 
-router.patch("/:commentId",auth(Role.USER, Role.ADMIN, Role.AUTHOR),commentController.updateComment);
+router.patch("/:commentId",auth(Role.USER, Role.ADMIN, Role.AUTHOR), commentController.updateComment);
 
-router.delete("/:commentId",auth(Role.USER, Role.ADMIN, Role.AUTHOR),commentController.deleteComment);
+router.delete("/:commentId", auth(Role.USER, Role.ADMIN, Role.AUTHOR), commentController.deleteComment);
 
-router.put("/:commentId/moderate",auth(Role.ADMIN),commentController.moderateComment);
+router.put("/:commentId/moderate", auth(Role.ADMIN), commentController.moderateComment);
 
 
 export const commentRoutes = router;
